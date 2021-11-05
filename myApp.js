2abb9074e9d263ee16a63e4b9cf0e6b48d398047
Dominik Wojnowski <dominatais@ais.pl>
2021-11-05 05:17:32 +0100

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
 console.log('Hello World');
 
+/*
 app.get('/', (req, res) => {
   res.send("Hello Express");
+})*/
+app.get('/', (req, res) => {
+  const path = __dirname + "/views/index.html"
+  console.log(`path=${path}`);
+  res.sendFile(path);
 })
 
  module.exports = app;
