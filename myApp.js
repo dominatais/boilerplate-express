b6b8555d99eca4c3cbc3c17efa15000d8cf8ac57
Dominik Wojnowski <dominatais@ais.pl>
2021-11-09 07:37:39 +0100

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 console.log('Hello World');
 app.use('/public', express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+})
 
 /*
 app.get('/', (req, res) => {
