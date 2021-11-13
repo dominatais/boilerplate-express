d6fc036e1d5c694d37266ac5d77897ba8aa70bcb
Dominik Wojnowski <dominatais@ais.pl>
2021-11-13 06:51:24 +0100

    Use body-parser to Parse POST Requests

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
+const bodyParser = require('body-parser');
 require('dotenv').config();
 var express = require('express');
 var app = express();
+
+bodyParser.urlencoded({extended: false});
 console.log('Hello World');
 app.use('/public', express.static(__dirname + '/public'));
 app.use((req, res, next) => {
